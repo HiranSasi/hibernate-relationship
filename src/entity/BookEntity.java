ed78185fa98e212f1b7ea489df48c10359747c21
HiranSasi <hiransasitha123@gmail.com>
2023-08-29 22:25:37 +1200

    one to many

--- src/entity/BookEntity.java
+++ src/entity/BookEntity.java
@@ -1,45 +1,36 @@
 package entity;
 
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.Cascade;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
-@ToString
 @Entity
-@Table(name = "Student")
+@Table(name = "Book")
 
-public class StudentEntity {
+public class BookEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String auther;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "Lap_ID",nullable = false)
-   
-    private LaptopEntity laptopEntity;
+@ManyToOne(cascade = CascadeType.ALL)
+@JoinColumn(name = "Student_id",nullable = false)
+    private StudentEntity studentEntity;
 
-@OneToMany(mappedBy = "studentEntity",targetEntity = BookEntity.class)
-    List<BookEntity>bookEntities;
 }
